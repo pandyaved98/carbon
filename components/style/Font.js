85a38b39d1eaa29a38c1293a0bbc70434b9299d1
Boy van Amstel <boy@boyvanamstel.nl>
2020-09-27 00:01:14 +0200

    Add fonts locally

--- components/style/Font.js
+++ components/style/Font.js
@@ -11,53 +11,42 @@ export default () => (
       @font-face {
         font-family: 'Iosevka';
         font-display: swap;
-        src: url('//cdn.jsdelivr.net/npm/@typopro/web-iosevka@3.7.5/TypoPRO-iosevka-term-bold.woff')
-          format('woff');
+        src: url('/static/fonts/TypoPRO-iosevka-term-bold.woff') format('woff');
       }
 
       @font-face {
         font-family: 'Monoid';
         font-display: swap;
-        src: url('//cdn.jsdelivr.net/npm/@typopro/web-monoid@3.7.5/TypoPRO-Monoid-Regular.woff')
-            format('woff2'),
-          url('//cdn.jsdelivr.net/npm/@typopro/web-monoid@3.7.5/TypoPRO-Monoid-Regular.woff')
-            format('woff');
+        src: url('/static/fonts/TypoPRO-Monoid-Regular.woff') format('woff2'),
+          url('/static/fonts/TypoPRO-Monoid-Regular.woff') format('woff');
       }
 
       @font-face {
         font-family: 'Fantasque Sans Mono';
         font-display: swap;
-        src: url('//cdn.jsdelivr.net/npm/@typopro/web-fantasque-sans-mono@3.7.5/TypoPRO-FantasqueSansMono-Regular.woff')
-            format('woff2'),
-          url('//cdn.jsdelivr.net/npm/@typopro/web-fantasque-sans-mono@3.7.5/TypoPRO-FantasqueSansMono-Regular.woff')
-            format('woff');
+        src: url('/static/fonts/TypoPRO-FantasqueSansMono-Regular.woff') format('woff2'),
+          url('/static/fonts/TypoPRO-FantasqueSansMono-Regular.woff') format('woff');
       }
 
       @font-face {
         font-family: 'Hack';
         font-display: swap;
-        src: url('//cdn.jsdelivr.net/font-hack/2.020/fonts/woff2/hack-regular-webfont.woff2?v=2.020')
-            format('woff2'),
-          url('//cdn.jsdelivr.net/font-hack/2.020/fonts/woff/hack-regular-webfont.woff?v=2.020')
-            format('woff');
+        src: url('/static/fonts/hack-regular-webfont.woff2') format('woff2'),
+          url('/static/fonts/hack-regular-webfont.woff') format('woff');
       }
 
       @font-face {
         font-family: 'Fira Code';
         font-display: swap;
-        src: url('//cdn.rawgit.com/tonsky/FiraCode/1.204/distr/woff2/FiraCode-Regular.woff2')
-            format('woff2'),
-          url('//cdn.rawgit.com/tonsky/FiraCode/1.204/distr/woff/FiraCode-Regular.woff')
-            format('woff');
+        src: url('/static/fonts/FiraCode-Regular.woff2') format('woff2'),
+          url('/static/fonts/FiraCode-Regular.woff') format('woff');
       }
 
       @font-face {
         font-family: 'JetBrains Mono';
         font-display: swap;
-        src: url('//cdn.jsdelivr.net/gh/JetBrains/JetBrainsMono/web/woff2/JetBrainsMono-Regular.woff2')
-            format('woff2'),
-          url('//cdn.jsdelivr.net/gh/JetBrains/JetBrainsMono/web/woff/JetBrainsMono-Regular.woff')
-            format('woff2');
+        src: url('/static/fonts/JetBrainsMono-Regular.woff2') format('woff2'),
+          url('/static/fonts/JetBrainsMono-Regular.woff') format('woff2');
       }
 
       /* latin */
@@ -66,8 +55,7 @@ export default () => (
         font-display: swap;
         font-weight: 500;
         src: local('IBM Plex Mono Medium Italic'), local('IBMPlexMono-MediumItalic'),
-          url(https://fonts.gstatic.com/s/ibmplexmono/v2/-F6sfjptAgt5VM-kVkqdyU8n1ioSJlR1gMoQPttozw.woff2)
-            format('woff2');
+          url('/static/fonts/-F6sfjptAgt5VM-kVkqdyU8n1ioSJlR1gMoQPttozw.woff2') format('woff2');
       }
 
       /* latin */
@@ -75,8 +63,7 @@ export default () => (
         font-family: 'Anonymous Pro';
         font-display: swap;
         src: local('Anonymous Pro Regular'), local('AnonymousPro-Regular'),
-          url(//fonts.gstatic.com/s/anonymouspro/v11/Zhfjj_gat3waL4JSju74E3n3cbdKJftHIk87C9ihfO8.woff2)
-            format('woff2');
+          url('/static/fonts/Zhfjj_gat3waL4JSju74E3n3cbdKJftHIk87C9ihfO8.woff2') format('woff2');
       }
 
       /* latin */
@@ -84,8 +71,7 @@ export default () => (
         font-family: 'Droid Sans Mono';
         font-display: swap;
         src: local('Droid Sans Mono Regular'), local('DroidSansMono-Regular'),
-          url(//fonts.gstatic.com/s/droidsansmono/v9/ns-m2xQYezAtqh7ai59hJVlgUn8GogvcKKzoM9Dh-4E.woff2)
-            format('woff2');
+          url('/static/fonts/ns-m2xQYezAtqh7ai59hJVlgUn8GogvcKKzoM9Dh-4E.woff2') format('woff2');
       }
 
       /* latin */
@@ -93,8 +79,7 @@ export default () => (
         font-family: 'Inconsolata';
         font-display: swap;
         src: local('Inconsolata Regular'), local('Inconsolata-Regular'),
-          url(//fonts.gstatic.com/s/inconsolata/v16/BjAYBlHtW3CJxDcjzrnZCIgp9Q8gbYrhqGlRav_IXfk.woff2)
-            format('woff2');
+          url('/static/fonts/BjAYBlHtW3CJxDcjzrnZCIgp9Q8gbYrhqGlRav_IXfk.woff2') format('woff2');
       }
 
       /* latin */
@@ -102,8 +87,7 @@ export default () => (
         font-family: 'Source Code Pro';
         font-display: swap;
         src: local('Source Code Pro'), local('SourceCodePro-Regular'),
-          url(//fonts.gstatic.com/s/sourcecodepro/v7/mrl8jkM18OlOQN8JLgasD5bPFduIYtoLzwST68uhz_Y.woff2)
-            format('woff2');
+          url('/static/fonts/mrl8jkM18OlOQN8JLgasD5bPFduIYtoLzwST68uhz_Y.woff2') format('woff2');
       }
 
       /* latin */
@@ -111,8 +95,7 @@ export default () => (
         font-family: 'Ubuntu Mono';
         font-display: swap;
         src: local('Ubuntu Mono'), local('UbuntuMono-Regular'),
-          url(//fonts.gstatic.com/s/ubuntumono/v7/ViZhet7Ak-LRXZMXzuAfkYgp9Q8gbYrhqGlRav_IXfk.woff2)
-            format('woff2');
+          url('/static/fonts/ViZhet7Ak-LRXZMXzuAfkYgp9Q8gbYrhqGlRav_IXfk.woff2') format('woff2');
       }
 
       /* latin */
@@ -120,8 +103,7 @@ export default () => (
         font-family: 'Space Mono';
         font-display: swap;
         src: local('Space Mono'), local('SpaceMono-Regular'),
-          url(https://fonts.gstatic.com/s/spacemono/v2/i7dPIFZifjKcF5UAWdDRYEF8RQ.woff2)
-            format('woff2');
+          url('/static/fonts/i7dPIFZifjKcF5UAWdDRYEF8RQ.woff2') format('woff2');
       }
     `}
   </style>
